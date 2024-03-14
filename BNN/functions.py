eb39597cf82224a0244a5e64de15935dea8cc742
DerRiesenPanda <105128555+DerRiesenPanda@users.noreply.github.com>
2024-03-14 19:48:00 +0100

    Update functions.py

--- BNN/functions.py
+++ BNN/functions.py
@@ -170,6 +170,208 @@ def count_elements_bigger_than_p(vector, p):
     """
     return sum(1 for x in vector if x <= p)
 
+def auc_curve(prediction, test_y, covariance_matrix, title):
+  """
+    Parameters:
+      prediction: the prediction made by the network
+      test_y: data to test on
+      covariance_matrix: covariance provided as an numpy array
+      title: title of the plot
+    
+    Returns:
+      plot of the calibration curve
+
+  """
+
+  nssr = [(prediction[i] - test_y[i]) @  (np.linalg.inv(covariance_matrix[i])) @ (prediction[i] - test_y[i]).T for i in range(len(covariance_matrix))]
+  predicted_probability = chi_square_cdf_probabilities(nssr)
+  observed_probability = [count_elements_bigger_than_p(nssr,p)/len(nssr) for p in nssr]
+  p_array = np.column_stack((predicted_probability.squeeze(), observed_probability))
+  curve = np.row_stack(([0,0],(p_array[p_array[:, 0].argsort()])))
+  plt.rcParams["figure.figsize"] = (10, 7)
+  plt.plot(curve[:,0], curve[:,1])
+  plt.axline([0, 0], [1, 1])
+  plt.title(title, color = 'blue')
+
+def return_auc_curve(prediction, test_y, covariance_matrix):
+
+    """
+    Parameters:
+      prediction: the prediction made by the network
+      test_y: data to test on
+      covariance_matrix: covariance provided as an numpy array
+
+    Returns: the calibration curve
+    """
+  nssr = [(prediction[i] - test_y[i]) @  (np.linalg.inv(covariance_matrix[i])) @ (prediction[i] - test_y[i]).T for i in range(len(covariance_matrix))]
+  predicted_probability = chi_square_cdf_probabilities(nssr)
+  observed_probability = [count_elements_bigger_than_p(nssr,p)/len(nssr) for p in nssr]
+  p_array = np.column_stack((predicted_probability.squeeze(), observed_probability))
+  curve = np.row_stack(([0,0],(p_array[p_array[:, 0].argsort()])))
+
+  return curve
+
+def distance(prediction, test_y, covariance_matrix):
+
+    """
+    Parameters:
+      prediction: the prediction made by the network
+      test_y: data to test on
+      covariance_matrix: covariance provided as an numpy array
+
+    Returns: the distance to the calibration curve and the area under the curve (AUC)
+
+    """
+
+  nssr = [(prediction[i] - test_y[i]) @  (np.linalg.inv(covariance_matrix[i])) @ (prediction[i] - test_y[i]).T for i in range(len(covariance_matrix))]
+  predicted_probability = chi_square_cdf_probabilities(nssr)
+  observed_probability = [count_elements_bigger_than_p(nssr,p)/len(nssr) for p in nssr]
+  p_array = np.column_stack((predicted_probability, observed_probability))
+  curve = np.row_stack(([0,0],(p_array[p_array[:, 0].argsort()])))
+  return round(np.sqrt(auc(curve[:,0],(curve[:,1]-curve[:,0])**2)),3), round(auc(curve[:,0],curve[:,1]))
+
+def fit_svi(model, guide, train_loader, model_name, lr=0.01, num_epochs=1000, plot=True):
+
+  """
+    Parameters:
+      model: a Baysian Neural Network
+      guide: the guide for the Network
+      train_loader: the training data as a PyTorch dataloader
+      model_name: model name for saving
+      lr: learning rate for SGD
+      num_epochs: number of epochs to train for
+      plot: whether or not to plot the loss curve
+    
+    Trains the model using SVI on the training data and saves the one with lowest loss
+  """
+
+  pyro.clear_param_store()
+  optimizer = pyro.optim.Adam({"lr": lr})
+
+  svi = SVI(model, guide, optimizer, Trace_ELBO())
+  progress_bar = trange(num_epochs)
+
+  loss_best = None
+
+  series = defaultdict(list)
+
+  for epoch in progress_bar:
+      loss=0
+      for batch_id, data in enumerate(train_loader):
+        loss += svi.step(data[0], data[1])
+
+      loss = loss / len(train_loader)
+      series["loss"].append(loss)
+      median = guide.median()
+
+      for name, value in median.items():
+        if value.numel() == 1:
+          series[name + " mean"].append(float(value))
+
+      if loss_best is None or loss < loss_best:
+        loss_best = loss
+        torch.save({"model" : model.state_dict(), "guide" : guide}, f'{model_name}.pt')
+        pyro.get_param_store().save(f'{model_name}_params.pt')
+
+      progress_bar.set_postfix(loss=f"{loss:.3f}")
+
+  if plot:
+    plt.figure(figsize=(6, 6))
+    for name, Y in series.items():
+      if name == "loss":
+        plt.plot(Y, "k--", label=name, zorder=0)
+      elif name.endswith(" mean"):
+        plt.plot(Y, label=name, zorder=-1)
+      else:
+        plt.plot(Y, label=name, alpha=0.5, lw=1, zorder=-2)
+        plt.xlabel("SVI step")
+        plt.title("loss, scalar parameters, and gradient norms")
+        plt.yscale("log")
+        plt.xscale("symlog")
+        plt.xlim(0, None)
+        plt.legend(loc="best", fontsize=8)
+        plt.tight_layout()
+
+def predict(model, guide, test_loader, num_samples = 500):
+
+ """
+    Parameters:
+      model: a Baysian Neural Network
+      guide: the guide for the Network
+      test_loader: the test data as a PyTorch dataloader
+      num_samples: number of samples to draw
+    
+    Returns:
+      pred: prediction on the test data
+      plus_error: vector of mean + 2 * standard deviation
+      minus_error: vector of mean - 2 * standard deviation
+      covariance: covariance matrix
+      real: the test data values
+  """
+
+
+  predictive = Predictive(model, guide = guide, num_samples = num_samples)
+  pred = []
+  plus_error= []
+  minus_error = []
+  covariance= []
+  real = []
+
+  for batch_id, data in enumerate(test_loader):
+    preds = predictive(data[0])
+    y_std = preds['obs'][:,0,:].detach().numpy().std(axis=0, ddof=1)
+    y_preds = preds['obs'][:,0,:].detach().numpy().sum(axis=0)/num_samples
+    plus_error.append(y_preds - 2* y_std)
+    minus_error.append(y_preds + 2* y_std)
+    pred.append(y_preds)
+
+    residuals = preds['obs'][:,0,:].detach().numpy() - y_preds
+    Sigma_y = (residuals.T @ residuals)/(num_samples - 1)
+
+    covariance.append(Sigma_y)
+
+    y_real = data[1]
+    real.append(y_real)
+
+  return pred, plus_error, minus_error, covariance, real
+
+def predict_mcmc(model, x_train, y_rain, x_test, num_samples = 50):
+
+   """
+    Parameters:
+      model: a Baysian Neural Network 
+      x_train: training data items
+      y_train: training data labels
+      x_test : the test data
+      num_samples: number of samples to draw
+    
+    Returns:
+      preds: prediction on the test data
+      plus_error_mcmc: vector of mean + 2 * standard deviation
+      minus_error_mcmc: vector of mean - 2 * standard deviation
+      covariance_mcmc: covariance matrix
+  """
+
+
+  pyro.clear_param_store()
+
+  # define MCMC sampler
+  nuts_kernel = NUTS(model, jit_compile=False)
+  mcmc = MCMC(nuts_kernel, num_samples= num_samples)
+  mcmc.run(x_train, y_train)
+
+
+  predictive = Predictive(model = model, posterior_samples = mcmc.get_samples())
+  preds_mcmc = predictive(x_test)
+  residuals = preds_mcmc['obs'] - preds_mcmc['obs'].mean(dim=0)
+  covariance_mcmc = (torch.transpose(residuals, 1,2) @ residuals)/(num_samples - 1)
+
+
+  minus_error_mcmc= (preds_mcmc['obs'].mean(dim=0)-2*preds_mcmc['obs'].std(dim=0)).numpy().flatten()
+  plus_error_mcmc= (preds_mcmc['obs'].mean(dim=0)+2*preds_mcmc['obs'].std(dim=0)).numpy().flatten()
+
+  return preds_mcmc['obs'].mean(dim=0), plus_error_mcmc, minus_error_mcmc, covariance_mcmc
+
 
 
 
